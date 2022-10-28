c63d141cbe0580f5c3a4c572687d6b94635278dd
Bruce Wilson <wilsonb@sm-01.cels.anl.gov>
2022-10-28 20:14:48 +0000

    Added ImageNet path to pod16.sh

--- vision/cnns/pytorch/train/rn50_pod16.sh
+++ vision/cnns/pytorch/train/rn50_pod16.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
 POPLAR_ENGINE_OPTIONS='{"opt.enableMultiAccessCopies":"false"}' \
-poprun --num-instances=8 --num-replicas=16 python3 train.py --config resnet50 --dataloader-worker 14 --dataloader-rebatch-size 256 $@
+poprun --num-instances=8 --num-replicas=16 python3 train.py --config resnet50 --imagenet-data-path /lambda_stor/habana/data/tensorflow/imagenet --dataloader-worker 14 --dataloader-rebatch-size 256 $@
